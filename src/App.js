c672a540f09bb054fc57ad36c34fc2c9fb6e7dc7
roig <roigrosa88@gmail.com>
2021-06-12 13:49:15 -0400

    got rid of unneeded starter files

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import './News.js';
-import '.NavBar.js'
+//import './News.js';
+//import '.NavBar.js'
 
 
 fetch('https://gnews.io/api/v4/{endpoint}27baa3eea030db2d0a5aff84b518799c');
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hello project</h1>
+
+
     </div>
+    
   );
 }
 
